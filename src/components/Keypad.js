f3549baf4b4ca8150cf14951e4b6f03d72fadf7e
JACKA <jackarcisse@gmail.com>
2024-05-27 13:27:40 +0300

    complete lab

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,20 @@
-// Code Keypad Component Here
+// components/Keypad.js
 
-function Keypad (){
+import React from 'react';
+
+class Keypad extends React.Component {
+  handleChange = () => {
+    console.log('Entering password...');
+  };
+
+  render() {
     return (
-        <div></div>
-    )
+      <input 
+        type="password" 
+        onChange={this.handleChange} 
+      />
+    );
+  }
 }
 
 export default Keypad;
